6d70a075ac2e39d013d5431fd35bdb0d04ebed70
CodiProgs <samuil.codiWork@mail.ru>
2024-09-01 12:50:03 +0300

    initial setup
    initialized from initial_project (https://github.com/CodiProgs/initial-project-nest-next-graphql)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { GraphQLModule } from '@nestjs/graphql'
+import { join } from 'path'
+import { AppResolver } from './app.resolver'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		GraphQLModule.forRoot<ApolloDriverConfig>({
+			driver: ApolloDriver,
+			autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+			sortSchema: true,
+			introspection: true,
+			playground: true,
+			context: ({ req, res }) => ({ req, res })
+		}),
+		ConfigModule.forRoot()
+	],
+	providers: [AppResolver]
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import * as cookieParser from 'cookie-parser'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule)
+
+	app.enableCors({
+		origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+		credentials: true
+	})
+	app.use(cookieParser())
+
+	await app.listen(process.env.PORT || 4200)
 }
-bootstrap();
+bootstrap()
